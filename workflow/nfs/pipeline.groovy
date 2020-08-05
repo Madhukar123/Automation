582fc01802d687eaa5aa869b484ae78dca603ec4
Madhukar123 <maddy.bidda@gmail.com>
2020-08-06 02:11:17 +0530

    Update pipeline.groovy

--- workflow/nfs/pipeline.groovy
+++ workflow/nfs/pipeline.groovy
@@ -1,3 +1,20 @@
 100.times {
   job('example'+ it) {} 
 }
+
+pipelineJob('DSL_Demo') {
+
+    def repo = 'https://github.com/Madhukar123/Automation.git'
+   
+    triggers {
+        scm('*/15 * * * *')
+    }
+    
+    definition {
+        cpsScm {
+          scm {
+            git(repo, 'master', { node -> node / 'extensions' << '' } )
+            }
+        }
+    }
+}
