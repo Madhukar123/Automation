1cfd5ffb4ca750649fd491194e851eeb88eb5065
Madhukar123 <maddy.bidda@gmail.com>
2020-08-06 02:31:10 +0530

    Update pipeline1.groovy

--- workflow/nfs/pipeline1.groovy
+++ workflow/nfs/pipeline1.groovy
@@ -2,9 +2,6 @@ pipelineJob('DSL_Demo') {
 
   def repo = 'https://github.com/Madhukar123/Automation.git' 
   def sshRepo = 'git@github.com:Madhukar123/Automation.git' 
-  triggers {
-        scm('*/15 * * * *')
-    }
     
     definition {
         cpsScm {
