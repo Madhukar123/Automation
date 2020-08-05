0db1fe643cd12cf1cb2b212ca71e12190e418093
Madhukar123 <maddy.bidda@gmail.com>
2020-08-06 02:47:03 +0530

    Update pipeline1.groovy

--- workflow/nfs/pipeline1.groovy
+++ workflow/nfs/pipeline1.groovy
@@ -20,3 +20,26 @@ pipelineJob('DSL_Demo') {
         }
     }
 }
+pipelineJob('DSL_Demo1') {
+
+      parameters {
+        booleanParam('FLAG', true)
+        choiceParam('OPTION', ['option 1 (default)', 'option 2', 'option 3'])
+    }
+  def repo = 'https://github.com/Madhukar123/Automation.git' 
+  def sshRepo = 'git@github.com:Madhukar123/Automation.git' 
+    
+    definition {
+        cpsScm {
+          scm {
+            git {
+              remote { url(repo) }
+              branches('master', '**/feature*')
+              scriptPath('workflow/nfs/pipeline.groovy')
+              extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+            }
+            }
+        }
+    }
+}
+
